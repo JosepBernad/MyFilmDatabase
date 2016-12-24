32f9f7a14d4864d3a52125f65464c9be1ea301b5
Josep Bernad Espinosa <jbernadsepinosa@gmail.com>
2016-12-24 16:59:00 +0100

    addFilm button finished (FloatingButton);

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
@@ -10,6 +10,7 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -40,7 +41,7 @@ public class MainActivity extends ListActivity implements View.OnClickListener
         setContentView(R.layout.main);
 
         /** (+) Add button */
-        ImageButton addButton = (ImageButton)findViewById(R.id.addButton);
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.addButton);
         addButton.setOnClickListener(this);
 
 
