c49a1d817053ace90b9082125c60344ee166b443
roger <roger32@gmail.com>
2017-01-11 18:10:28 +0100

    Navigation Drawer al new film

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/NewFilmActivity.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/NewFilmActivity.java
@@ -1,11 +1,22 @@
 package com.example.pr_idi.mydatabaseexample;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,6 +43,10 @@ public class NewFilmActivity extends AppCompatActivity {
     RatingBar ratingBar;
     TextView ratingText;
     Button submitButton;
+
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -53,6 +68,18 @@ public class NewFilmActivity extends AppCompatActivity {
 
         listenerForRatingBar();
         listenerForSubmitButton();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_new);
+
+        mDrawerList = (ListView) findViewById(R.id.navListNew);
+        LayoutInflater myinflater = getLayoutInflater();
+        ViewGroup myHeader = (ViewGroup)myinflater.inflate(R.layout.headerlayout, mDrawerList, false);
+        mDrawerList.addHeaderView(myHeader, null, false);
+        addDrawerItems();
+
+        setupDrawer();
 
         filmData = new FilmData(this);
         filmData.open();
@@ -172,6 +199,92 @@ public class NewFilmActivity extends AppCompatActivity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
+    }
+    private void addDrawerItems() {
+        String[] filmArray = {"My Films", "Add Film", "Help", "About"};
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filmArray);
+        mDrawerList.setAdapter(mAdapter);
+
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 1:
+                        finish();
+                        break;
+                    case 2: //cas Add Film
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case 3: //cas Help
+                        finish();
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        Intent i = new Intent(NewFilmActivity.this, HelpActivity.class);
+                        startActivity(i);
+                        break;
+                    case 4: //cas About
+                        finish();
+                        mDrawerLayout.closeDrawer(GravityCompat.START);
+                        i = new Intent(NewFilmActivity.this, AboutActivity.class);
+                        startActivity(i);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Es crida quan el drawer esta completament obert */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Menu");
+                invalidateOptionsMenu();
+            }
+
+            /** Es crida quan el drawer esta completament tancat */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle("New Film");
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Controlar els clicks de la action bar aqui.
+        int id = item.getItemId();
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
 
